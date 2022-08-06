f0003b4ef13bcee5cdd1498d41bbd632f0f13efe
Idris Westbrook <westbrook.idris@gmail.com>
2022-08-06 16:57:31 -0400

    add global object

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 console.log('Hello Word');
 console.log(global);
+{/* <ref *1> Object [global] {
+  global: [Circular *1],
+  queueMicrotask: [Function: queueMicrotask],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  structuredClone: [Function: structuredClone],
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  performance: Performance {
+    nodeTiming: PerformanceNodeTiming {
+      name: 'node',
+      entryType: 'node',
+      startTime: 0,
+      duration: 50.99039699882269,
+      nodeStart: 3.0726099982857704,
+      v8Start: 4.1783920004963875,
+      bootstrapComplete: 39.96339400112629,
+      environment: 23.716761000454426,
+      loopStart: -1,
+      loopExit: -1,
+      idleTime: 0
+    },
+    timeOrigin: 1659819215190.77
+  },
+  fetch: [AsyncFunction: fetch]
+} */}
