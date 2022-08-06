757695316ab2a318b393c8ac5470e33747284854
Idris Westbrook <westbrook.idris@gmail.com>
2022-08-06 17:44:34 -0400

    add common core modules - os, path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-console.log('Hello Word');
-console.log(global);
-{/* <ref *1> Object [global] {
+console.log('Hello World');
+// console.log(global);
+/* <ref *1> Object [global] {
   global: [Circular *1],
   queueMicrotask: [Function: queueMicrotask],
   clearImmediate: [Function: clearImmediate],
@@ -19,16 +19,41 @@ console.log(global);
       name: 'node',
       entryType: 'node',
       startTime: 0,
-      duration: 50.99039699882269,
-      nodeStart: 3.0726099982857704,
-      v8Start: 4.1783920004963875,
-      bootstrapComplete: 39.96339400112629,
-      environment: 23.716761000454426,
+      duration: 194.82135999947786,
+      nodeStart: 44.38122799992561,
+      v8Start: 64.31922800093889,
+      bootstrapComplete: 174.9443470016122,
+      environment: 111.24100300297141,
       loopStart: -1,
       loopExit: -1,
       idleTime: 0
     },
-    timeOrigin: 1659819215190.77
+    timeOrigin: 1659819878950.572
   },
   fetch: [AsyncFunction: fetch]
-} */}
+} */
+
+// Common Core Modules (CommonJS)
+const os = require('os');
+const path = require('path');
+
+console.log(os.type()); // Darwin (MacOS)
+console.log(os.version()); // Darwin Kernel Version 20.6.0: Tue Jun 21 20:50:28 PDT 2022; root:xnu-7195.141.32~1/RELEASE_X86_64
+console.log(os.homedir()); // /Users/idris
+
+console.log(__dirname); // /Users/idris/Desktop/projects/project-nodefun
+console.log(__filename); // /Users/idris/Desktop/projects/project-nodefun/server.js
+
+// Path
+console.log(path.dirname(__filename)); // /Users/idris/Desktop/projects/project-nodefun
+console.log(path.basename(__filename)); // server.js
+console.log(path.extname(__filename)); // .js
+
+console.log(path.parse(__filename));
+/* {
+    root: '/',
+    dir: '/Users/idris/Desktop/projects/project-nodefun',
+    base: 'server.js',
+    ext: '.js',
+    name: 'server'
+} */
