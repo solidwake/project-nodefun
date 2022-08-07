7498b29cbc9832c6ed37d962b49a429d29c007a4
Idris Westbrook <westbrook.idris@gmail.com>
2022-08-06 23:06:58 -0400

    add stream, mkdir, rmdir, existsSync

---
+++ dir.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// Exists Sync method checks if a directory exists. If so, it will not overwrite the directory
+if(!fs.existsSync('./new',)) {
+    // mkdir creates a new directory
+    fs.mkdir('./new', (err) => {
+        if (err) throw err;
+        console.log('Directory created.');
+    })
+}
+
+if(fs.existsSync('./new',)) {
+    // rmdir removes a new directory
+    fs.rmdir('./new', (err) => {
+        if (err) throw err;
+        console.log('Directory removed.');
+    })
+}
+
+process.on('uncaughtException', err => {
+    console.error(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
---
+++ stream.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+// For larger files, Stream can be used for better performance
+const rs = fs.createReadStream(path.join(__dirname, 'files', 'lorem.txt'), 'utf8');
+
+const ws = fs.createWriteStream(path.join(__dirname, 'files', 'newlorem.txt'));
+
+/* rs.on('data', (dataChunk) => {
+    ws.write(dataChunk);
+}) */
+
+rs.pipe(ws);
