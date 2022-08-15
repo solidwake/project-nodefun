ce72e29f9031e1fb89758892162385308a5b4a10
Idris Westbrook <westbrook.idris@gmail.com>
2022-08-14 23:06:33 -0400

    serve index and newpage

--- express.js
+++ express.js
@@ -7,8 +7,13 @@ dotenv.config({ path: './config/config.env' });
 
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
-})
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/newpage(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'newpage.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
