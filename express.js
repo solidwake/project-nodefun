cad61ae7255ba16118006c91f8020e3ac96cb55d
Idris Westbrook <westbrook.idris@gmail.com>
2022-08-17 17:41:58 -0400

    add error handling

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middleware/events');
+const errorHandler = require('./middleware/errorhandler')
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
@@ -16,7 +17,7 @@ app.use(logger);
 const whitelist = ['https://www.google.com', /* 'https://www.yoursite.com', 'https://yoursite.com', */ 'http://127.0.0.1:5500', 'http://locatlhost:5000']
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
@@ -24,7 +25,7 @@ const corsOptions = {
     },
     optionsSuccessStatus: 200
 }
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Built in Middleware to handle url encoded data (form data)
 // 'content-type: application/x-www-form-urlencoded'
@@ -78,5 +79,12 @@ app.get('/chain(.html)?', [one, two, three]);
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
+
+/* app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send(err.message);
+}) */
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
