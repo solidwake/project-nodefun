27d7cd0ebc92755401c0dbbe2299b9d3de5fc88c
Idris Westbrook <westbrook.idris@gmail.com>
2022-08-26 22:05:55 -0400

    add async crud operations

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,18 +1,12 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
-
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const fsPromises = require('fs').promises;
-const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
-    const foundUser = usersDB.users.find(person => person.username === user);
+    const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401); // HTTP 401 means Unauthorized
     // Evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
@@ -35,14 +29,11 @@ const handleLogin = async (req, res) => {
             { expiresIn: '1d' } // Typically an extended period (ex. 1 day)
         );
         // Save the refresh token in the DB, which allows creation of a logout route that invalidates the refresh token when a user logs out
-        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 }); //! In production, use  { secure: true }, but { secure: true } does not work with Thunder Client when testing the refresh token
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); //! In production, use  { secure: true }, but { secure: true } does not work with Thunder Client when testing the refresh token
         res.json({ accessToken });
     } else {
         res.sendStatus(401);
--- controllers/logoutcontroller.js
+++ controllers/logoutcontroller.js
@@ -1,9 +1,4 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
     //! Be sure to delete the access token on the client side
@@ -12,20 +7,16 @@ const handleLogout = async (req, res) => {
     const refreshToken = cookies.jwt;
 
     // Is the refresh token in the DB?
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // { maxAge } is not necessary when deleting a cookie 
         return res.sendStatus(204);
     }
-    
+
     // Delete refesh token in DB
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = { ...foundUser, refreshToken: '' };
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(result);
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); //! In production, use  { secure: true }
     res.sendStatus(204);
